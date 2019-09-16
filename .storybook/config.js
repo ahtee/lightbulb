2a4697072fe69c0f2666ff8c0d1c74d9ea6e1ae4
ahtee <otteb1@nku.edu>
2019-09-15 20:58:50 -0400

    adding final configs for today

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,11 @@
-import { configure } from '@storybook/react';
+import { configure, addParameters } from '@storybook/react';
+import { themes } from '@storybook/theming';
+
+addParameters({
+  options: {
+    theme: themes.light
+  }
+});
 
 // automatically import all files ending in *.stories.js
-configure(require.context('../stories', true, /\.stories\.js$/), module);
+configure(require.context('./stories', true, /\.stories\.js$/), module);
--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
 import bundleSize from 'rollup-plugin-bundle-size';
+import typescript from 'rollup-plugin-typescript';
 
 export default {
   input: 'src/index.js',
@@ -10,13 +11,17 @@ export default {
   },
   external: [
     'prop-types',
+    '@types/react',
     'react',
+    '@types/react-dom',
     'react-dom',
     'react-router-dom',
+    '@types/styled-components',
     'styled-components',
     'styled-theming'
   ],
   plugins: [
+    typescript(),
     resolve(),
     babel({
       exclude: 'node_modules/**'
